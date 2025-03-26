25f0fa295aae731ad62ff32f3776103cf642ef86
unknown <239040@TECH.WYB.AC.LK>
2025-03-26 19:38:18 +0530

    first commit

---
+++ aa.py
@@ -0,0 +1,6 @@
+num=int(input("Enter Your Number : "))
+print (num)
+num1=int(input("Enter Your Number : "))
+print (num)
+sum = num1+num
+print (sum)
---
+++ input.py
@@ -0,0 +1,4 @@
+num=int(input("Enter Your Number : "))
+print (num)
+name=str(input("Enter Your Name : "))
+print (name)
